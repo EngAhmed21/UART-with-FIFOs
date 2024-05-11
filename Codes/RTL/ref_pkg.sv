629e83990049dd854e0df53773f9e01c773401eb
Ahmed Mohamed <90782588+EngAhmed21@users.noreply.github.com>
2024-05-12 01:35:56 +0300

    Delete Codes/RTL directory

--- Codes/RTL/ref_pkg.sv
+++
@@ -1,8 +0,0 @@
-package ref_pkg;
-    import states_pkg::*;
-    import param_pkg::*;
-
-    state_e tx_cs, rx_cs;
-    bit [SCNT_BIT-1:0] tx_s_cnt, rx_s_cnt;
-    bit s_tick;
-endpackage
--- Codes/RTL/states_pkg.sv
+++
@@ -1,3 +0,0 @@
-package states_pkg;
-    typedef enum bit [1:0] {IDLE, START, DATA, STOP} state_e;
-endpackage
