635d49943e72b8402416650c1bc1a05b6f67c458
Ahmed Mohamed <90782588+EngAhmed21@users.noreply.github.com>
2024-07-01 22:03:21 +0300

    Update uart_sys_sequence_pkg.sv

--- RTL/Design + UVM-based Verification/UART System/uart_sys_sequence_pkg.sv
+++ RTL/Design + UVM-based Verification/UART System/uart_sys_sequence_pkg.sv
@@ -48,4 +48,31 @@ package uart_sys_sequence_pkg;
             end
         endtask
     endclass
+
+    class uart_sys_full_sequence extends uvm_sequence #(uart_sys_seq_item);
+        `uvm_object_utils(uart_sys_full_sequence)
+
+        uart_sys_seq_item full_seq_item;
+
+        function new(string name = "uart_sys_full_sequence");
+            super.new(name);
+        endfunction
+
+        task pre_body;
+            full_seq_item = uart_sys_seq_item::type_id::create("full_seq_item");
+        endtask
+
+        task body;
+            repeat(10000) begin
+                start_item(full_seq_item);
+                    full_seq_item.rst_n   = 1;
+                    full_seq_item.rx      = $random();
+                    full_seq_item.rd_uart = 0;
+                    full_seq_item.wr_uart = 1;
+                    full_seq_item.rst_n   = 1;
+                    full_seq_item.w_data  = $random();
+                finish_item(full_seq_item);
+            end
+        endtask
+    endclass
 endpackage
